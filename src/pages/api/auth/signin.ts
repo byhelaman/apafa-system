43c6678ed82d08fa2ba9dac4e0a67935e4e47892
Helaman <byhelaman@gmail.com>
2024-08-31 18:20:18 -0500

    handle session errors

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -12,10 +12,26 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   });
 
   if (error) {
-    return new Response(JSON.stringify({ message: "Ha habido un problema con su solicitud." }), { status: error.status });
+    return new Response(JSON.stringify({ 
+      message: "Ha habido un problema con su solicitud.",
+      redirect: "/signout" 
+    }), { status: error.status });
   }
 
   const { access_token, refresh_token } = data.session;
+
+  const { error: errorSession } = await supabase.auth.setSession({
+    access_token,
+    refresh_token
+  })
+
+  if (errorSession) {
+    return new Response(JSON.stringify({ 
+      message: "Ha habido un problema con su solicitud.",
+      redirect: "/signout" 
+    }), { status: errorSession.status });
+  }
+
   cookies.set("sb-access-token", access_token, {
     path: "/",
     secure: true,
